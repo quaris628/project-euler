f481edde6e2df03fb885e758d59fc556095d6818
Quaris <quaris314@gmail.com>
2022-08-06 14:24:24 -0500

    Completed problem 1 - Multiples of 3 or 5

---
+++ src/archive/Prob1_Mult3Or5.java
@@ -0,0 +1,40 @@
+package archive;
+
+
+import lib.Multiples;
+import main.Problem;
+
+import static lib.Multiples.sumMultsUpTo;
+
+/**
+ * If we list all the natural numbers below 10 that are
+ * multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Prob1_Mult3Or5 extends Problem {
+
+    public final static int MAX = 1000;
+
+    @Override
+    public int run() {
+
+        int sum = 0;
+        for (int i = 3; i < MAX; i+=3) {
+            sum += i;
+        }
+        log(sum);
+
+        int sumMult3 = sumMultsUpTo(3, MAX);
+        log("Sum mults 3", sumMult3);
+
+        int sumMult5 = sumMultsUpTo(5, MAX);
+        log("Sum mults 5", sumMult5);
+
+        int sumMult15 = sumMultsUpTo(15, MAX);
+        log("Sum mults 15", sumMult15);
+
+        return sumMult3 + sumMult5 - sumMult15;
+    }
+}
