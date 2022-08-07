670624618a86287f38f1aa89a9dabb820f0ea86a
Quaris <quaris314@gmail.com>
2022-08-07 16:14:13 -0500

    Completed problem 5 - Smallest multiple

---
+++ src/archive/Prob5_SmallestMultiple.java
@@ -0,0 +1,17 @@
+package archive;
+
+import lib.Multiples;
+import lib.Naturals;
+import main.Problem;
+
+public class Prob5_SmallestMultiple extends Problem {
+    @Override
+    protected int run() {
+        long lcm = Multiples.lcm(Naturals.generate(20));
+        if (lcm > Integer.MAX_VALUE) {
+            logTime();
+            throw new ArithmeticException("Result " + lcm + " is greater than Integer.MAX_VALUE");
+        }
+        return (int)lcm;
+    }
+}
