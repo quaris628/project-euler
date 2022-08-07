d4a1b2c548e2795288bc72f173794a09b0a111ac
Quaris <quaris314@gmail.com>
2022-08-07 13:24:06 -0500

    Completed problem 3 - Largest prime factor

---
+++ src/archive/Prob3_LargestPrimeFactor.java
@@ -0,0 +1,39 @@
+package archive;
+
+import lib.Primes;
+import main.Problem;
+
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+ What is the largest prime factor of the number 600851475143 ?
+
+ */
+public class Prob3_LargestPrimeFactor extends Problem {
+
+    @Override
+    protected int run() {
+        long number = 600851475143L;
+        int numSqrt = (int)Math.sqrt(number);
+
+        // we want to transverse the primes in descending order
+        // so use a sorted tree set with O(log(n)) add/remove/contains, sorting in reverse
+        SortedSet<Integer> primes = new TreeSet<Integer>(Collections.reverseOrder());
+        Primes.generateIn(primes, numSqrt);
+
+        logTime("Prime gen");
+
+        for (int prime : primes) {
+            if (number % prime == 0) {
+                return prime;
+            }
+        }
+
+        return -1;
+    }
+}
