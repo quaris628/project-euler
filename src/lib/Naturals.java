550d4bcdd8d828aa44b490ba8560285a33c96c23
Quaris <quaris314@gmail.com>
2022-08-07 20:23:55 -0500

    Added time complexity comments to library class Naturals

--- src/lib/Naturals.java
+++ src/lib/Naturals.java
@@ -2,10 +2,16 @@ package lib;
 
 public class Naturals {
 
+    /**
+     * O(1)
+     */
     public static int sumUpTo(int n) {
         return n * (n + 1) / 2;
     }
 
+    /**
+     * O(upTo)
+     */
     public static int[] generate(int upTo) {
         int[] naturals = new int[upTo];
         for (int i = 1; i <= upTo; i++) {
