67d8c7108864555673cc33d2bcd063f145b7d307
Quaris <quaris314@gmail.com>
2022-08-10 00:41:59 -0500

    Amending mistake in previous commit

--- src/archive/ActiveProblem.java
+++
@@ -1,8 +0,0 @@
-package archive;
-
-import main.Problem;
-import problem.Prob8_LargestProductInASeries;
-
-public class ActiveProblem {
-    public final static Problem ACTIVE_PROBLEM = new Prob8_LargestProductInASeries();
-}
